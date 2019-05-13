41218cef3f3dde0fecc16afcf7dee228848f4687
Raul de Melo <melo.raulf@gmail.com>
2019-05-13 23:20:26 +0200

    Fix tests

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,34 +4,38 @@ import styled from 'styled-components';
 import Info from './Info';
 import Box from './Box';
 
-import { ContainerConsumer } from './ContainerContext';
+import { ContainerContext } from './ContainerContext';
 const SpanName = styled.span`
   color: #f77f00;
 `;
 
 export default class Body extends Component {
+  static contextType = ContainerContext;
   state = {
-    message: ''
+    message: '',
+    companyName: ''
   };
 
-  componentDidUpdate = oldProps => {
-    if (oldProps.companyName !== this.props.companyName) {
-      this.setState({ message: 'Greetings! :) (did update)' });
+  componentDidUpdate = () => {
+    if (this.state.companyName !== this.context.companyName) {
+      this.setState({
+        companyName: this.context.companyName,
+        message: 'Greetings! :) (did update)'
+      });
     }
   };
+
   render() {
     return (
-      <ContainerConsumer>
-        {value => (
-          <Box color="#cd84f1" content="Body.js">
-            <h2>
-              It's good to see you at <SpanName>{value.companyName}</SpanName>
-            </h2>
-            <p>Some extra information bellow:</p>
-            <Info />
-          </Box>
-        )}
-      </ContainerConsumer>
+      <Box color="#cd84f1" content="Body.js">
+        {this.state.message && <h2>{this.state.message}</h2>}
+        <h2>
+          It's good to see you at <SpanName>{this.state.companyName}</SpanName>
+        </h2>
+
+        <p>Some extra information bellow:</p>
+        <Info />
+      </Box>
     );
   }
 }
