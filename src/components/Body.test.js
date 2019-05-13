f36f4ce3edf045d0970771cc6a054d4fc938c064
Raul de Melo <melo.raulf@gmail.com>
2019-05-13 22:42:18 +0200

    Add another test

--- src/components/Body.test.js
+++ src/components/Body.test.js
@@ -5,11 +5,17 @@ import Body from './Body';
 
 const defaultProps = {};
 
-const mountComponent = props =>
-  mount(<Body {...defaultProps} {...props} />);
+const mountComponent = props => mount(<Body {...defaultProps} {...props} />);
 
 describe('<Body />', () => {
   it('renders without error', () => {
     mountComponent();
+  });
+
+  it('renders companyName passed as props', () => {
+    const companyName = 'YoungCapital';
+    const $wrapper = mountComponent({ companyName });
+
+    expect($wrapper.text()).toMatch(companyName);
   });
 });
